4d68d79326f27997d3e3dc7dff4df508600b38b3
xiaop1ng <784516419@qq.com>
2021-06-30 16:31:31 +0800

    twoSum

--- src/src/main/java/com/xiaoping/twosum/Solution.java
+++ src/src/main/java/com/xiaoping/twosum/Solution.java
@@ -36,8 +36,7 @@ public class Solution {
     public int[] twoSum(int[] nums, int target) {
         int size = nums.length;
         for (int i = 0; i < size; i++) {
-            for (int j = i; j < size; j++) {
-                if (i==j) continue;
+            for (int j = i+1; j < size; j++) {
                 if (nums[i] + nums[j] == target) {
                     return new int[]{i, j};
                 }
